543e473877306ec405f999acaa29c6c91011cb13
Jose Galo <fjng01234567812@gmail.com>
2022-07-22 17:03:33 -0600

    Inicializando primer proyecto en java

---
+++ src/prograiiprimerprograma/PrograIIPrimerPrograma.java
@@ -0,0 +1,13 @@
+
+package prograiiprimerprograma;
+
+
+public class PrograIIPrimerPrograma {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
