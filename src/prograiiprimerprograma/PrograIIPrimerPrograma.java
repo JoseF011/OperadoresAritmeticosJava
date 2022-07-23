3c15a1fb575ca73e0f12c29ea2967e63d6171fb4
Jose Galo <fjng01234567812@gmail.com>
2022-07-22 19:54:46 -0600

    Se agregó una prueba

--- src/prograiiprimerprograma/PrograIIPrimerPrograma.java
+++ src/prograiiprimerprograma/PrograIIPrimerPrograma.java
@@ -7,7 +7,9 @@ public class PrograIIPrimerPrograma {
    
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        System.out.println("Hola mundo");
+        System.out.println("Primera prueba de ingresar esto");
+        System.out.println("Let's do it");
     }
     
 }
